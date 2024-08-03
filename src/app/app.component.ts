d32003cd761e351ba5d92341fef548cad804c83c
Nour <nouralashkar.cs@gmail.com>
2024-08-03 07:55:27 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,AboutComponent,SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
